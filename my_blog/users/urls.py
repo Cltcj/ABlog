7f355bfd6226b0c609f9ecef556755397d91b9c9
Cltcj <lvtingchi@126.com>
2022-06-22 23:39:38 +0800

    图片验证码、短信验证码

--- my_blog/users/urls.py
+++ my_blog/users/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ImageCodeView, SmsCodeView
 
 urlpatterns = [
     # 参数1：路由
     # 参数2：视图函数
     # 参数3：路由名，方便通过reverse来获取路由
+
     path('register/',RegisterView.as_view(),name='register'),
+
+    # 图片验证码的路由
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
+
+    # 短信发送
+    path('smscode/',SmsCodeView.as_view(),name='smscode'),
 ]
