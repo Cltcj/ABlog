64aa5ae69d39c394032c1339f5e8bb36f7a19278
Cltcj <lvtingchi@126.com>
2022-06-23 14:04:41 +0800

    登陆、退出、忘记密码功能

--- my_blog/users/urls.py
+++ my_blog/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import RegisterView, ImageCodeView, SmsCodeView
+from users.views import RegisterView, ImageCodeView, SmsCodeView, LoginView, LogoutView, ForgetPasswordView
 
 urlpatterns = [
     # 参数1：路由
@@ -13,4 +13,20 @@ urlpatterns = [
 
     # 短信发送
     path('smscode/',SmsCodeView.as_view(),name='smscode'),
+
+    # 登录路由
+    path('login/', LoginView.as_view(), name='login'),
+
+    #退出登录
+    path('logout/',LogoutView.as_view(),name='logout'),
+
+    # 忘记密码
+    path('forgetpassword/', ForgetPasswordView.as_view(), name='forgetpassword'),
+
+    # 个人中心
+    # path('center/', UserCenterView.as_view(), name='center'),
+    #
+    # # 写博客的路由
+    # path('writeblog/', WriteBlogView.as_view(), name='writeblog'),
+
 ]
